d9df11b2f36e4cf01dc2c3f39e3f7dae9c4cf6f3
swiftwill <prc-lh138-18@PRC-LH138-18s-iMac.local>
2016-07-11 14:27:13 -0500

    Exercise2_2ndtime
    2nd version

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, Pokie!"
+
+let p2 = str
+
+let s:String = "Where ya goin?"
+
+let i:Int = 21
+
+var f:Float = 3.14
+
+var d:Double = 99.99
+
+var b:Bool = true
+
+var ctr = 5
+
+var results = 10
+
+
+    str
+    s
+    results
+    ctr = (ctr - 1)
+
+if ctr == 4
+{b = false}
+
+b
+
+
